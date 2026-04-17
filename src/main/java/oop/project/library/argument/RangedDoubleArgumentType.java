0f8afc251dba71861ebe3706dcb2cb2a236e6c0f
Joel Samuel <joel13samuel@gmail.com>
2026-04-17 10:50:49 -0400

    refactored ranged double argument type to use validator

--- src/main/java/oop/project/library/argument/RangedDoubleArgumentType.java
+++ src/main/java/oop/project/library/argument/RangedDoubleArgumentType.java
@@ -2,23 +2,17 @@ package oop.project.library.argument;
 
 public class RangedDoubleArgumentType implements ArgumentType<Double> {
 
-    private final DoubleArgumentType doubleArgumentType;
-    private final double min;
-    private final double max;
+    private final ValidatedArgumentType<Double> validatedArgumentType;
 
     public RangedDoubleArgumentType(double min, double max) {
-        this.doubleArgumentType = new DoubleArgumentType();
-        this.min = min;
-        this.max = max;
+        this.validatedArgumentType = new ValidatedArgumentType<>(
+                new DoubleArgumentType(),
+                number -> number >= min && number <= max,
+                "Value must be between " + min + " and " + max + "."
+        );
     }
 
     public Double parse(String value) throws RuntimeException {
-        double number = doubleArgumentType.parse(value);
-
-        if (number < min || number > max) {
-            throw new RuntimeException("Value must be between " + min + " and " + max + ".");
-        }
-
-        return number;
+        return validatedArgumentType.parse(value);
     }
 }
