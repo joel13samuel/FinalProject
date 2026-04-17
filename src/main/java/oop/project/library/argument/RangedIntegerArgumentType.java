47e6cb8596bc8dda93d9f3f96e61d1dbae599c0d
Joel Samuel <joel13samuel@gmail.com>
2026-04-17 10:50:00 -0400

    refactored ranged integer argument type to use validator

--- src/main/java/oop/project/library/argument/RangedIntegerArgumentType.java
+++ src/main/java/oop/project/library/argument/RangedIntegerArgumentType.java
@@ -2,23 +2,17 @@ package oop.project.library.argument;
 
 public class RangedIntegerArgumentType implements ArgumentType<Integer> {
 
-    private final IntegerArgumentType integerArgumentType;
-    private final int min;
-    private final int max;
+    private final ValidatedArgumentType<Integer> validatedArgumentType;
 
     public RangedIntegerArgumentType(int min, int max) {
-        this.integerArgumentType = new IntegerArgumentType();
-        this.min = min;
-        this.max = max;
+        this.validatedArgumentType = new ValidatedArgumentType<>(
+                new IntegerArgumentType(),
+                number -> number >= min && number <= max,
+                "Value must be between " + min + " and " + max + "."
+        );
     }
 
     public Integer parse(String value) throws RuntimeException {
-        int number = integerArgumentType.parse(value);
-
-        if (number < min || number > max) {
-            throw new RuntimeException("Value must be between " + min + " and " + max + ".");
-        }
-
-        return number;
+        return validatedArgumentType.parse(value);
     }
 }
