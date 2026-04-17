059d2d3c86f1dec34ac7a414a1db14fc5680a817
Joel Samuel <joel13samuel@gmail.com>
2026-04-17 11:02:15 -0400

    updated string parsing to match the new exception setup

--- src/main/java/oop/project/library/argument/StringArgumentType.java
+++ src/main/java/oop/project/library/argument/StringArgumentType.java
@@ -2,7 +2,7 @@ package oop.project.library.argument;
 
 public class StringArgumentType implements ArgumentType<String> {
 
-    public String parse(String value) throws RuntimeException {
+    public String parse(String value) throws ArgumentParseException {
         return value;
     }
 }
